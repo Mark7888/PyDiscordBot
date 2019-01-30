a23ac26488e782a37bc9c37672e0b0de98fa07c9
Mark7888 <44124704+Mark7888@users.noreply.github.com>
2019-01-30 20:09:56 +0100

    ocommands

--- app/ocommands.py
+++ app/ocommands.py
@@ -2,5 +2,5 @@ from reload import reload
 def ocommand(message, msgargs, prefix, ownerID, adminID, consol_prefix):
 	if message.content.upper().startswith(prefix + "RELOAD"):
 		if message.author.id == ownerID:
-			reload(message, client, consol_prefix)
+			reload(message, consol_prefix)
 	#elif message.content.upper().startswith(prefix + ""):
