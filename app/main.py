18afbe62e23722076e46592cea296bfd432ddbd8
Mark7888 <44124704+Mark7888@users.noreply.github.com>
2019-01-30 20:02:03 +0100

    prefixes, command list, status file, on_ready

--- app/main.py
+++ app/main.py
@@ -23,3 +23,33 @@ Token = Token[1:len(Token)-1]
 ownerID = ownerID[1:len(ownerID)-1]
 adminID = "531927224840880141"
 prefix = prefix[1:len(prefix)-1]
+
+#set cmd prefixes
+log_prefix = Fore.MAGENTA + "log - " + Fore.RESET
+consol_prefix = Fore.YELLOW + Back.RED + Style.BRIGHT + "console" + Back.RESET + " - " + Style.RESET_ALL
+server_prefix = Fore.GREEN + "[Server]: " + Fore.RESET
+
+#read the command's list
+commandfile = open("../config/commands.list", "r+")
+commandlist = commandfile.read().split(" ")
+
+#open status file
+try:
+    stat = open("../config/status.file", "r+")
+except:
+    stat = open("../config/status.file", "w").close()
+    stat = open("../config/status.file", "r+")
+
+#It runs if the bot is ready
+@client.event
+async def on_ready():
+    if stat.read() == "RESTART":
+        print(consol_prefix + "Reload completed.")
+        stat.close()
+        open("../config/status.file", "w").close()
+    else:
+        print('\n' + Fore.GREEN + "Logged in as:")
+        print(Fore.CYAN + client.user.name + " <" + client.user.id + ">")
+        print(Fore.GREEN + "---------------------------")
+        print('\n' + Fore.RESET)
+        stat.close()
