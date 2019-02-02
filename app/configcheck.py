f5a712251581125c77fa489b65103f32a1d2a21e
Mark7888 <l.mark7888@gmail.com>
2019-02-02 21:20:38 +0100

    Set config writing

--- app/configcheck.py
+++ app/configcheck.py
@@ -1,20 +1,16 @@
 from configparser import ConfigParser
+config = ConfigParser()
 def configcheck(warn_prefix):
     try:
         open('../config/config.ini', "r").close()
-        config = ConfigParser()
-        config.read('../config/config.ini')
-        if config.get('BotConfig', 'Token') or config.get('BotConfig', 'ownerID') or config.get('BotConfig', 'default_prefix') == '""':
-            print(warn_prefix + "Please check the config file in the 'config' folder!")
-            exit()
 
     except:
         file = open('../config/config.ini', "w")
-        file.write('''[BotConfig]
-Token: ""
-ownerID: ""
-default_prefix: ""''')
-        file.close()
-
+        config['BotConfig'] = {
+            'Token': '""',
+            'ownerID': '""',
+            'default_prefix': '"!"'
+        }
+        config.write(file)
         print(warn_prefix + "The Config file was created in the 'config' folder.")
         exit()
