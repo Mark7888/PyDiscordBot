0b5ad0c59cd28e3abf2f616f666691009f14cb8a
Mark7888 <l.mark7888@gmail.com>
2019-02-13 18:01:14 +0100

    Delete

--- app/reload.py
+++
@@ -1,8 +0,0 @@
-from subprocess import call
-def reload(message, consol_prefix):
-	print(consol_prefix + "Reloading...")
-	stat = open("../config/status.file", "w")
-	stat.write("RESTART")
-	stat.close()
-	call(['python3', 'main.py'])
-	exit()
