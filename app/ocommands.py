d51c263e1649dc8d4065f87de28624104ab08b2f
Mark7888 <l.mark7888@gmail.com>
2019-02-13 18:01:14 +0100

    Delete

--- app/ocommands.py
+++
@@ -1,6 +0,0 @@
-from reload import reload
-def ocommand(message, msgargs, prefix, ownerID, adminID, consol_prefix):
-	if message.content.upper().startswith(prefix + "RELOAD"):
-		if message.author.id == ownerID:
-			reload(message, consol_prefix)
-	#elif message.content.upper().startswith(prefix + ""):
--- app/scommands.py
+++
@@ -1,1 +0,0 @@
-def scommand(message, msgargs, prefix):
