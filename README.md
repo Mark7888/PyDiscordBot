c5f401136b0cfece879ec59d368c3f17858f0bce
Mark7888 <l.mark7888@gmail.com>
2019-03-27 21:35:39 +0100

    Created auto updater

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ PyDiscordBot is a simple discord chatbot written in Python.
 ### Installation
 
 This program requires [Python3](https://www.python.org/downloads/release/python-366/) to run. (Doesn't work in python 3.7)
+GitPython requires git being installed on the system, and accessible via system's PATH.
 
 Install the python packages.
 
@@ -14,6 +15,7 @@ Install the python packages.
 $ python -m pip install discord
 $ python -m pip install configparser
 $ python -m pip install colorama
+$ python -m pip install gitpython
 ```
 
 License
---
+++ update/update.py
@@ -0,0 +1,46 @@
+from shutil import copytree as copyfile
+from shutil import copy2
+from os import system, path
+from os import remove as osremove
+from colorama import init
+from colorama import Fore, Back, Style
+init(autoreset=True)
+# commandline prefix
+warn_prefix = Fore.YELLOW + Back.RED + Style.BRIGHT + "WARN" + Style.RESET_ALL + " - "
+# check git
+try:
+    from git import Repo
+except:
+    print(warn_prefix + "GitPython requires git being installed on the system, and accessible via system's PATH.")
+    exit()
+# delete old bot update folder
+if path.exists("./BotUpdate"):
+    system('rmdir /S /Q "{}"'.format("./BotUpdate"))
+# clone repo
+Repo.clone_from("https://github.com/Mark7888/PyDiscordBot", "./BotUpdate")
+system('rmdir /S /Q "{}"'.format("./BotUpdate/.git"))
+# check version
+if path.exists("../app.version"):
+    oldversionfile = open("./BotUpdate/app.version", "r")
+    newversionfile = open("../app.version", "r")
+    oldversion = oldversionfile.read()
+    newversion = newversionfile.read()
+    if oldversion == newversion:
+        print("You have already installed the newest version.")
+        exit()
+
+# delete files
+if path.exists("../app"):
+    system('rmdir /S /Q "{}"'.format("../app"))
+#if path.exists("../update"):
+    # system('rmdir /S /Q "{}"'.format("../update"))
+if path.exists("../config/lang"):
+    system('rmdir /S /Q "{}"'.format("../config/lang"))
+if path.exists("../config/commands.list"):
+    osremove("../config/commands.list")
+
+# copy new files
+copyfile("./BotUpdate/app", "../app")
+# copyfile("./BotUpdate/update", "../update")
+copyfile("./BotUpdate/config/lang", "../config/lang")
+copy2("./BotUpdate/config/commands.list", "../config")
