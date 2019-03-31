9cf87bb4b2e3e86627b856cc3fc8ecc162d77c5c
Mark7888 <l.mark7888@gmail.com>
2019-03-31 12:26:17 +0200

    Auto Update Linux

--- update/update.py
+++ update/update.py
@@ -13,12 +13,31 @@ try:
 except:
     print(warn_prefix + "GitPython requires git being installed on the system, and accessible via system's PATH.")
     exit()
+
+# check os type
+if platform.system() == "Windows":
+    systemsname = "windows"
+elif platform.system() == "Linux":
+    systemsname = "linux"
+else:
+    print(warn_prefix + "The auto updater is not compatible with this operation system!")
+    exit()
+
+# remove in linux : "sudo rm -rf .git"
 # delete old bot update folder
 if path.exists("./BotUpdate"):
-    system('rmdir /S /Q "{}"'.format("./BotUpdate"))
+    if systemsname == "windows":
+        system('rmdir /S /Q "{}"'.format("./BotUpdate"))
+    elif systemsname == "linux":
+        system('sudo rm -rf ./BotUpdate')
 # clone repo
 Repo.clone_from("https://github.com/Mark7888/PyDiscordBot", "./BotUpdate")
-system('rmdir /S /Q "{}"'.format("./BotUpdate/.git"))
+
+if systemsname == "windows":
+    system('rmdir /S /Q "{}"'.format("./BotUpdate/.git"))
+elif systemsname == "linux":
+    system('sudo rm -rf ./BotUpdate/.git')
+
 # check version
 if path.exists("../app.version"):
     oldversionfile = open("./BotUpdate/app.version", "r")
@@ -26,20 +45,32 @@ if path.exists("../app.version"):
     oldversion = oldversionfile.read()
     newversion = newversionfile.read()
     if oldversion == newversion:
-        print("You have already installed the newest version.")
+        print(Fore.YELLOW + "You have already installed the newest version.")
         exit()
 
 # delete files
-if path.exists("../app"):
-    system('rmdir /S /Q "{}"'.format("../app"))
-if path.exists("../update/update.py"):
-    osremove("../update/update.py")
-if path.exists("../config/lang"):
-    system('rmdir /S /Q "{}"'.format("../config/lang"))
-if path.exists("../config/commands.list"):
-    osremove("../config/commands.list")
-if path.exists("../app.version"):
-    osremove("../app.version")
+if systemsname == "windows":
+    if path.exists("../app"):
+        system('rmdir /S /Q "{}"'.format("../app"))
+    if path.exists("../update/update.py"):
+        osremove("../update/update.py")
+    if path.exists("../config/lang"):
+        system('rmdir /S /Q "{}"'.format("../config/lang"))
+    if path.exists("../config/commands.list"):
+        osremove("../config/commands.list")
+    if path.exists("../app.version"):
+        osremove("../app.version")
+elif systemsname == "linux":
+    if path.exists("../app"):
+        system('sudo rm -rf ../app')
+    if path.exists("../update/update.py"):
+        osremove("../update/update.py")
+    if path.exists("../config/lang"):
+        system('sudo rm -rf ../config/lang')
+    if path.exists("../config/commands.list"):
+        osremove("../config/commands.list")
+    if path.exists("../app.version"):
+        osremove("../app.version")
 
 # copy new files
 copyfile("./BotUpdate/app", "../app")
@@ -50,6 +81,9 @@ copy2("./BotUpdate/app.version", "../app.version")
 
 # delete bot update folder
 if path.exists("./BotUpdate"):
-    system('rmdir /S /Q "{}"'.format("./BotUpdate"))
+    if systemname == "windows":
+        system('rmdir /S /Q "{}"'.format("./BotUpdate"))
+    elif systemname == "linux":
+        system('sudo rm -rf ./BotUpdate')
 
-print("Bot successfully updated.")
+print(Fore.GREEN + "Bot successfully updated.")
